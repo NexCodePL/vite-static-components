8cd107e0dd35ebaea7d2e5d963e73d2847ca1229
Alexander Stefański <alex@aps-studio.eu>
2023-09-26 12:25:25 +0200

    v1.0.16

--- src/Link.tsx
+++ src/Link.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { Link as LinkRaw } from "react-router-dom";
+import { Link as LinkRaw, LinkProps as LinkPropsRaw } from "react-router-dom";
 import { StaticRouteBase } from "@nexcodepl/vite-static";
 import { RouteDataContextContextType, useRouteData } from "./routeDataContext.js";
+
+export type LinkPropsOptional = Partial<LinkPropsRaw>;
 
 export interface LinkProps {
     className?: string;
     children?: JSX.Element | string | null | JSX.Element[];
     to: string | { id: string };
     onClick?: () => void;
+    propsRaw?: LinkPropsOptional;
 }
 
 export type LinkComponent = React.FC<LinkProps>;
 
 export function getLinkComponent(routeDataContext: RouteDataContextContextType): LinkComponent {
-    const Link: React.FC<LinkProps> = ({ className, children, to, onClick }) => {
+    const Link: React.FC<LinkProps> = ({ className, children, to, onClick, propsRaw }) => {
         const routeData = useRouteData(routeDataContext);
 
         return (
             <LinkRaw
+                {...propsRaw}
                 to={getLink(to, routeData?.routesMap)}
                 className={className}
                 onClick={() => {
